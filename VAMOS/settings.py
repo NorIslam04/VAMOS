7d8e357470fe5489ba4d1525ffcc04e7a323b84b
NorIslam04 <islamaoudi18@gmail.com>
2024-11-23 22:40:06 +0100

    afficher les photos stocker dans base de donnes + configurations

--- VAMOS/settings.py
+++ VAMOS/settings.py
@@ -125,6 +125,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"VAMOS/static") #SOURCE
 ] 
 
+
+MEDIA_URL = 'photos/'  # URL pour accéder aux fichiers médias
+MEDIA_ROOT = os.path.join(BASE_DIR, '')  # Chemin où les fichiers médias sont stockés
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
