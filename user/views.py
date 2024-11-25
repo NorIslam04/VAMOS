32090472ccb95124760d1d65cfe6e67acdc741e8
NorIslam04 <islamaoudi18@gmail.com>
2024-11-26 00:03:29 +0100

    add a comments

--- user/views.py
+++ user/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import redirect
 
 # Ajouter un utilisateur
 def add_user(request):
-    if request.method == 'POST':
+    if request.method == 'POST': 
         first_name = request.POST.get('first_name')  # Récupère les valeurs POST
         last_name = request.POST.get('last_name')
         try:
@@ -19,7 +19,7 @@ def add_user(request):
         user.save()
         
         # Stocker l'ID de l'utilisateur dans la session
-        request.session['id_user'] = user.id #
+        request.session['id_user'] = user.id #pour utiliser dans fonction profil et cette instruction generer un cookie cote client
         return HttpResponseRedirect(reverse('profil'))
     
     return render(request, 'user/sign_in.html')
@@ -46,7 +46,7 @@ def profil(request):
 
 def reset_id_user(request): # Réinitialiser l'ID de l'utilisateur, modifier les cookies
     if request.method == 'POST':
-        request.session['id_user'] = 0  # Réinitialise id_user
+        request.session['id_user'] = 0  # Réinitialise id_user cote serveur(cookie)
         return redirect('login')  # Redirige vers la page de connexion
     return HttpResponse("Méthode non autorisée", status=405)
 
@@ -57,7 +57,7 @@ def login(request):
 
             try:
                 user = User.objects.get(first_name=first_name, last_name=last_name)
-                request.session['id_user'] = user.id
+                request.session['id_user'] = user.id # affecter la valeur de l'id_user si on a besion de l'utiliser dans une autre fonction
                 return HttpResponseRedirect(reverse('profil'))
             except User.DoesNotExist:
                 return HttpResponse("Utilisateur introuvable.")
