6c171b07abae354dc45923dc93d31b0916aa7099
NorIslam04 <islamaoudi18@gmail.com>
2024-11-27 16:58:42 +0100

    add login page

--- usr/urls.py
+++ usr/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.signin, name='signin'),  # name='person' is the name of the url pattern, qui est utilisé pour les liens inverses et appelé la fcntion views.person
+    path('login', views.login, name='login'),
 ]
--- usr/views.py
+++ usr/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def signin(request):
     return render(request, 'usr/signin.html')
+def login(request):
+    return render(request, 'usr/login.html')
