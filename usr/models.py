c2478288b49c7643a0888f68b8267fddba071b1e
NorIslam04 <islamaoudi18@gmail.com>
2024-11-27 18:36:04 +0100

    add user admin + input (form)

--- usr/models.py
+++ usr/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Usr(models.Model):
+    username=models.CharField(max_length=50)
+    email=models.EmailField(max_length=50)
+    password=models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.username}"
+
+
--- usr/views.py
+++ usr/views.py
@@ -1,6 +1,29 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from .models import Usr
 
-def signin(request):
-    return render(request, 'usr/signin.html')
+
 def login(request):
     return render(request, 'usr/login.html')
+
+# Ajouter un utilisateur
+def signin(request):
+    if request.method == 'POST': 
+        username = request.POST.get('username')  # Récupère les valeurs POST
+        email = request.POST.get('email')
+        password=request.POST.get('password')
+        
+        # Création de l'utilisateur
+        user = Usr.objects.create(username=username, email=email, password=password)
+        user.save()
+        print("user a ete bien enregestrer")
+        # Stocker l'ID de l'utilisateur dans la session
+        #request.session['id_user'] = user.id #pour utiliser dans fonction profil et cette instruction generer un cookie cote client
+        return HttpResponseRedirect(reverse('prof'))
+    
+    return render(request, 'usr/signin.html')
+
+def profil(req):
+    return render(req,'usr/prof.html')
+
