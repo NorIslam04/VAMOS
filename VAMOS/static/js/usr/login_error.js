0d0d65049088ba99abb168eef332c473cbe8f870
NorIslam04 <islamaoudi18@gmail.com>
2024-11-28 21:36:31 +0100

    add js in static file for error msg

--- VAMOS/static/js/usr/login_error.js
+++ VAMOS/static/js/usr/login_error.js
@@ -1,24 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("login-form");
-    const passwordInput = document.getElementById("password");
-    const errorMessage = document.getElementById("error-message");
-    
-    if (form) {
-      form.addEventListener("submit", function (event) {
-        event.preventDefault(); // Prevents form submission for this demo
-    
-        // Simulated validation
-        if (passwordInput && errorMessage) {
-          if (passwordInput.value !== "expectedPassword") {
-            passwordInput.classList.add("error"); // Add error class for styling
-            errorMessage.style.display = "block"; // Display the error message
-          } else {
-            passwordInput.classList.remove("error");
-            errorMessage.style.display = "none"; // Hide the error message when valid
-            form.submit(); // Submit the form if validation is successful
-          }
-        }
-      });
-    }
-  });
-  
+
+document.addEventListener('DOMContentLoaded', function() { //DOMContentLoaded oubligation d'attendre que le DOM soit chargé pour exécuter le script
+  const form = document.getElementById("login-form");
+  const passwordInput = document.getElementById("password");
+  const errorMessage = document.getElementById("error-message");
+  if (form) {
+    form.addEventListener("submit", function (event) {
+      console.log("Form submitted");
+      event.preventDefault();
+      
+      console.log("Password value:", passwordInput.value);
+      
+      if (passwordInput.value !== "qq") {
+        console.log("Password is incorrect");
+        passwordInput.classList.add("error");
+        errorMessage.style.display = "block";
+      } else {
+        console.log("Password is correct");
+        passwordInput.classList.remove("error");
+        errorMessage.style.display = "none";
+        form.submit();
+      }
+    });
+  }
+});
